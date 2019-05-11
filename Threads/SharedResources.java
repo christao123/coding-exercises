33d66748fa7358c9e3f52c7beaeb583a65344bb9
Danilo Del Busso <danilo.delbusso1@gmail.com>
2019-05-11 19:29:16 +0100

    Add Shared Resources with buffer example

--- Threads/SharedResources.java
+++ Threads/SharedResources.java
@@ -1,12 +1,35 @@
-package Threads;
+import java.util.Random;
 
+/**
+Two threads share same array. They access is at random different
+intervals and therefore modify the same resource unintentionally
+*/
 public class SharedResources{
+    private static int[] buffer = new int[10];
+    private static Random rand = new Random();
+
     public static void main(String[] args){
-        int[] buffer = new int[10];
-        Thread p = new PrintP(buffer);
-        Thread q = new PrintP(buffer);
+        Thread p = new MyThread();
+        Thread q = new MyThread();
         p.start();
         q.start();
     }
 
+    public static class MyThread extends Thread{
+        @Override
+        public void run() {
+            try{
+                Thread.sleep(rand.nextInt(100));
+            } catch (InterruptedException e){
+                e.printStackTrace();
+               }
+            if(buffer[0] == 3){
+                System.out.println("Element is already 3, can't change it.");
+            }
+            else{
+            System.out.println("Changing element to 3 from " + buffer[0]);
+            buffer[0] = 3;
+            }
+        }
+    }
 }
