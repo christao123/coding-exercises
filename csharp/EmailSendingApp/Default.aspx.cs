5cb352c0b03fa44f6c2fb0b44b2429c2cf344528
Danilo Del Busso <danilo.delbusso1@gmail.com>
2019-07-03 21:12:29 +0100

    Implement Basic Page with email sending and file creation

--- csharp/EmailSendingApp/Default.aspx.cs
+++ csharp/EmailSendingApp/Default.aspx.cs
@@ -4,18 +4,38 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using static WebApplication_.FileManager;
+using WebApplication_.CustomUtils;
+
 
 namespace WebApplication_
 {
     public partial class _Default : Page
     {
-        private string rootPath = HttpContext.Current.Server.MapPath("~");
+        private readonly string _rootPath = HttpContext.Current.Server.MapPath("~");
         protected void Page_Load(object sender, EventArgs e)
         {
-           string t = new FileManager().ReadFromFile(rootPath + "/App_Data/file.txt");
+           string t = new FileManager().ReadFromFile(_rootPath + "/App_Data/file.txt");
            TextBox1.Text = t;
-           new FileManager().WriteToFile(rootPath + "/App_Data/new_file.txt", "oioioi");
+
+           string bigString = "Imma Win That Coffee\n";
+           for (int i = 0; i < 10; i++)
+           {
+               bigString += bigString;
+           }
+
+           new FileManager().WriteToFile(
+               _rootPath + "/App_Data/new_file.txt", 
+               bigString);
+
+           string[] attachmentsPaths =
+           {
+               _rootPath + "App_Data/new_file.txt",
+               _rootPath + "App_Data/images/logo.PNG"
+           };
+           new EmailSender(_rootPath + "App_Data/configs/defaultEmailCfg.xml").SendEmail(
+               "delbussodanilo98@gmail.com", "Test Message",
+               "this is a message", attachmentsPaths
+               );
         }
     }
 }
